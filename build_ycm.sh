b0d5c1ca5e618353d93aeefb9bf9eb262894676b
Matteo Riondato <riondato@acm.org>
2015-12-22 10:37:53 -0500

    Include all commands, so it is easier to build YCM

--- build_ycm.sh
+++ build_ycm.sh
@@ -1,2 +1,26 @@
 #! /bin/sh
-cmake -G "Unix Makefiles" -DEXTERNAL_LIBCLANG_PATH=/opt/local/libexec/llvm-3.6/lib/libclang.dylib -DPYTHON_LIBRARY=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib -DPYTHON_INCLUDE_DIR=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/Headers .  ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+CLANG_DYLIB=/opt/local/libexec/llvm-3.7/lib/libclang.dylib
+PYTHON_DYLIB=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib
+PYTHON_INCLUDE=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/Headers
+if [ ! -f ${CLANG_DYLIB} ]; then
+    echo "ERROR:" ${CLANG_DYLIB} " not found: update CLANG_DYLIB variable."
+    exit 1
+fi
+if [ ! -f ${PYTHON_DYLIB} ]; then
+    echo "ERROR:" ${PYTHON_DYLIB} " not found: update PYTHON_DYLIB variable."
+    exit 1
+fi
+if [ ! -d ${PYTHON_INCLUDE} ]; then
+    echo "ERROR:" ${PYTHON_INCLUDE} " not found: update PYTHON_INCLUDE variable."
+    exit 1
+fi
+if [ -d ycmd_build ]; then
+    rm -rf ycmd_build
+fi
+mkdir ycmd_build && \
+cd ycmd_build && \
+cmake -G "Unix Makefiles" -DEXTERNAL_LIBCLANG_PATH=${CLANG_DLIB} -DPYTHON_LIBRARY=${PYTHON_DYLIB} -DPYTHON_INCLUDE_DIR=${PYTHON_INCLUDE} .  ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp && \
+make ycm_support_libs && \
+make && \
+rm -rf ycmd_build && \
+cd ..
