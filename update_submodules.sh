2af40957bcd19d6217d1c56b7c536a3361bff8f7
Matteo Riondato <riondato@acm.org>
2017-03-25 10:52:34 -0400

    Sync submodules.

--- update_submodules.sh
+++ update_submodules.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git submodule sync --recursive
-git submodule update --remote --merge --recursive
+git submodule update --recursive #--remote --merge
 #%git submodule foreach --recursive git submodule update --init --recursive
 git commit -a -m "Sync submodules."
 git push
