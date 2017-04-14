b332960a41343ec6da5c08cb6a07595f7147abbb
Matteo Riondato <riondato@acm.org>
2017-04-14 17:25:30 -0400

    Only commit bundle.

--- update_submodules.sh
+++ update_submodules.sh
@@ -2,7 +2,7 @@
 git submodule sync --recursive
 git submodule update --init --recursive # --remote --merge
 git submodule update --init --remote --merge
-git commit -a -m "Sync submodules."
+git commit bundle -m "Sync submodules."
 git push
 
 ./build_command_t.sh
