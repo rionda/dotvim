fe4f0aab3cf24b7fc5f58ca35eb5a9d098a7d794
Matteo Riondato <riondato@acm.org>
2017-05-15 09:04:10 -0400

    Switch YCM to use Python3.

--- build_ycm.sh
+++ build_ycm.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 CLANG_DYLIB=/opt/local/libexec/llvm-4.0/lib/libclang.dylib
-PYTHON_DYLIB=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib
-PYTHON_INCLUDE=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/Headers
+PYTHON_DYLIB=/opt/local/Library/Frameworks/Python.framework/Versions/3.6/lib/libpython3.6.dylib
+PYTHON_INCLUDE=/opt/local/Library/Frameworks/Python.framework/Versions/3.6/Headers
 if [ ! -f ${CLANG_DYLIB} ]; then
     echo "ERROR:" ${CLANG_DYLIB} " not found: update CLANG_DYLIB variable."
     exit 1
@@ -19,8 +19,8 @@ if [ -d ycm_build ]; then
 fi
 mkdir ycm_build && \
 cd ycm_build && \
-cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DEXTERNAL_LIBCLANG_PATH=${CLANG_DYLIB} -DPYTHON_LIBRARY=${PYTHON_DYLIB} -DPYTHON_INCLUDE_DIR=${PYTHON_INCLUDE} .  ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp && \
-cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DEXTERNAL_LIBCLANG_PATH=${CLANG_DYLIB} -DUSE_CLANG_COMPLETER=y -DPYTHON_LIBRARY=${PYTHON_DYLIB} -DPYTHON_INCLUDE_DIR=${PYTHON_INCLUDE} --build . --target ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp # --config Release
+cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DEXTERNAL_LIBCLANG_PATH=${CLANG_DYLIB} -DUSE_PYTHON2=OFF -DPYTHON_LIBRARY=${PYTHON_DYLIB} -DPYTHON_INCLUDE_DIR=${PYTHON_INCLUDE} .  ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp && \
+cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DEXTERNAL_LIBCLANG_PATH=${CLANG_DYLIB} -DUSE_PYTHON2=OFF -DUSE_CLANG_COMPLETER=y -DPYTHON_LIBRARY=${PYTHON_DYLIB} -DPYTHON_INCLUDE_DIR=${PYTHON_INCLUDE} --build . --target ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp # --config Release
 #make ycm_support_libs && \
 make && \
 cd .. && \
