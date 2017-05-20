aa2b3df1ef1a773d87a836140c97c49238eb51b4
Matteo Riondato <riondato@acm.org>
2017-05-20 18:55:39 -0400

    set LD_LIBRARY_PATH for YCM build, so it can find libclang.

--- build_ycm.sh
+++ build_ycm.sh
@@ -2,8 +2,8 @@
 UNAME=`uname`
 if [ ${UNAME} = "FreeBSD" ]; then
     cd bundle/YouCompleteMe && \
-    python2 ./install.py --clang-completer --system-libclang \
-    --system-boost
+    env LD_LIBRARY_PATH=/usr/local/llvm40/lib python2 ./install.py \
+    --clang-completer --system-libclang --system-boost
 elif [ ${UNAME} = "Dariwn" ]; then
     CLANG_DYLIB=/opt/local/libexec/llvm-4.0/lib/libclang.dylib
     PYTHON_DYLIB=/opt/local/Library/Frameworks/Python.framework/Versions/3.6/lib/libpython3.6.dylib
