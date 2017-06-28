6f52f8d69fc67e9985e485a1edc6b134819bff0d
Matteo Riondato <riondato@acm.org>
2017-06-28 08:50:42 -0400

    Reorder update operations to make (IMHO) more sense: first we sync the URLs (recursively), then we update /init the submodules, then update / init the submodules in the submodules, recursively. My one question is whether we should switch the first and second operations.

--- update_submodules.sh
+++ update_submodules.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-git submodule sync --recursive
-git submodule update --init --recursive # --remote --merge
-git submodule update --init --remote --merge
-git commit bundle -m "Sync submodules."
+git submodule sync --recursive && \
+git submodule update --init --remote --merge && \
+git submodule update --init --recursive && \ # --remote --merge
+git commit bundle -m "Sync submodules." && \
 git push
 
 ./build_command_t.sh
