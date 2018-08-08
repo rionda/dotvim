a1f9ff419e017d1bf21fc4ed825c35324d738bda
Matteo Riondato <riondato@acm.org>
2018-08-08 10:18:07 -0400

    Turn off gpg signing for commits from update-submodules, so it can be used from cron.

--- update_submodules.sh
+++ update_submodules.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+git pull && \
+git submodule sync && \
 git submodule update --init --remote --merge && \
 git submodule sync --recursive && \
-git submodule update --init --recursive && \
-git commit bundle -m "Sync submodules." && \
+git commit --no-gpg-sign bundle -m "Sync submodules." && \
+git submodule update --init --merge --recursive && \
 git push
 
 ./build_command_t.sh
