7cd0a97f9e55923dbea31374279e268746cb8588
Matteo Riondato <riondato@acm.org>
2020-02-26 18:52:06 -0500

    Slight reorganization of the order of operations.

--- update_submodules.sh
+++ update_submodules.sh
@@ -2,10 +2,10 @@
 git pull && \
 git submodule sync && \
 git submodule update --init --remote --merge && \
-git submodule sync --recursive && \
 git commit --no-gpg-sign bundle -m "Sync submodules." && \
-git submodule update --init --merge --recursive && \
-git push
+git push && \
+git submodule sync --recursive && \
+git submodule update --init --recursive # --merge 
 
 ./build_command_t.sh
 ./build_ycm.sh
