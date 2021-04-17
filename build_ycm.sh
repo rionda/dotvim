eae021c32260fc83e13b6bf40d3e4d4314fc94e3
Matteo Riondato <riondato@acm.org>
2021-04-17 12:11:14 -0400

    Activate the java completer and use the quiet build.

--- build_ycm.sh
+++ build_ycm.sh
@@ -3,7 +3,7 @@ UNAME=`uname`
 cd pack/matteo/start/YouCompleteMe
 if [ ${UNAME} = "FreeBSD" ]; then
     env LD_LIBRARY_PATH=/usr/local/llvm11/lib python3.8 ./install.py \
-    --clang-tidy --clang-completer --system-libclang --system-boost
+    --clang-tidy --clang-completer --java-completer --system-libclang --system-boost --quiet
 elif [ ${UNAME} = "Darwin" ]; then
     CLANG_DYLIB=/opt/local/libexec/llvm-11/lib/libclang.dylib
     LLVM_DYLIB=/opt/local/libexec/llvm-11/lib/libLLVM.dylib
@@ -19,9 +19,11 @@ elif [ ${UNAME} = "Darwin" ]; then
     if [ -d ycm_build ]; then
     rm -rf ycm_build
     fi
-    EXTRA_CMAKE_ARGS="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DEXTERNAL_LIBCLANG_PATH=${CLANG_DYLIB} -DPATH_TO_LLVM_ROOT=${LLVM_ROOT}" python3 ./install.py --clang-completer --clang-tidy
+    EXTRA_CMAKE_ARGS="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DEXTERNAL_LIBCLANG_PATH=${CLANG_DYLIB} -DPATH_TO_LLVM_ROOT=${LLVM_ROOT}" python3 ./install.py \\
+    --clang-completer --clang-tidy --java-completer --quiet
 elif [ ${UNAME} = "Linux" ]; then
-    python3 ./install.py --clang-completer --clang-tidy
+    python3 ./install.py \\
+    --clang-completer --clang-tidy --java-completer --quiet
 else
     echo "Fix $0 to build on you OS." >&2
     exit 1
