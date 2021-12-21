d7eb5107e25bfbdd67b9478bcfe71ac6aee965e5
Matteo Riondato <rionda@acm.org>
2021-12-21 10:48:03 -0500

    Fix indent.

--- update_submodules.sh
+++ update_submodules.sh
@@ -17,7 +17,7 @@ oldycmcmt=`getYCMcommit`
 (echo "## Updating/Initing and merging submodules from their remote" &&  \
     git submodule update --init --remote --merge) || exit 1
 (echo "## Committing update " && \
-git commit --no-gpg-sign pack/matteo/ -m "Sync submodules.") || \
+    git commit --no-gpg-sign pack/matteo/ -m "Sync submodules.") || \
     exit 1
 (echo "## Pushing " && git push) || exit 1
 (echo "## Syncing submodules recursively" &&  \
