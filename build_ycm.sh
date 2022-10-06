2b580ea461740484e7028cc043153da854f556db
Matteo Riondato <rionda@acm.org>
2022-10-06 10:32:04 -0400

    Ensure all libraries are copied. I don't know why this should be needed. Also, update to llvm-15.

--- build_ycm.sh
+++ build_ycm.sh
@@ -5,7 +5,7 @@ if [ ${UNAME} = "FreeBSD" ]; then
     env LD_LIBRARY_PATH=/usr/local/llvm13/lib python3.8 ./install.py \
     --clang-tidy --clang-completer --java-completer --system-libclang --quiet
 elif [ ${UNAME} = "Darwin" ]; then
-    LLVM_ROOT=/opt/local/libexec/llvm-14/
+    LLVM_ROOT=/opt/local/libexec/llvm-15/
     CLANG_DYLIB=${LLVM_ROOT}lib/libclang.dylib
     LLVM_DYLIB=${LLVM_ROOT}lib/libLLVM.dylib
     if [ ! -f ${CLANG_DYLIB} ]; then
@@ -18,8 +18,8 @@ elif [ ${UNAME} = "Darwin" ]; then
     fi
     EXTRA_CMAKE_ARGS="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DEXTERNAL_LIBCLANG_PATH=${CLANG_DYLIB} -DPATH_TO_LLVM_ROOT=${LLVM_ROOT}" python3 ./install.py \
     --clang-completer --clang-tidy --java-completer --quiet
-    # For some reason, the library is not copied, but it is needed.
-    cp ${CLANG_DYLIB} third_party/ycmd/third_party/clang/lib/
+    # For some reason, the libraries are not copied, but they are needed.
+    cp ${CLANG_DYLIB} ${LLVM_DYLIB} third_party/ycmd/third_party/clang/lib/
 elif [ ${UNAME} = "Linux" ]; then
     python3 ./install.py \
     --clang-completer --clang-tidy --java-completer --quiet
